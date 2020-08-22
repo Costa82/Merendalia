684d22146aa101e98f3c6ac888f1aa1147cccb05
Costa82 <costa_torito@hotmail.com>
2020-08-22 21:21:53 +0200

    Primeros cambios imágenes en la página del blog

--- views/default/contents/content_blog_merendalia.php
+++ views/default/contents/content_blog_merendalia.php
@@ -8,32 +8,49 @@
 
 </div>
 
-<div class="entradas_blog">
+<h4 class="titulo_entradas">ENTRADAS</h4>
 
-	<h4 class="titulo_entradas">ENTRADAS</h4>
+<div class="entradas_blog">
 	
 	<div class="entrada_blog">
-		<p><b><a href='corto_hummus'>CORTO HUMMUS</a></b></p>
+		<a href='cortometraje%20restaurante%20video'><p class="titulo_entrada_blog"><b>CORTO HUMMUS</b></p>
+		<img src='./views/default/img/Portada.jpg'
+		title='amigos fiesta merendalia' alt='amigos fiesta merendalia'
+		class='img_articulo_blog' /></a>
 		<p>05 de julio de 2020</p>
 	</div>
 	
 	<div class="entrada_blog">
-		<p><b><a href='reserva_el_espacio_en_ferias_de_valladolid'>¡RESERVA EL ESPACIO EN FERIAS DE VALLADOLID 2019!</a></b></p>
+		<a href='reserva_el_espacio_en_ferias_de_valladolid'><p class="titulo_entrada_blog"><b>RESERVA EL ESPACIO EN FERIAS DE VALLADOLID 2019</b></p>
+		<img src='./views/default/img/Portada.jpg'
+		title='amigos fiesta merendalia' alt='amigos fiesta merendalia'
+		class='img_articulo_blog' /></a>
 		<p>21 de agosto de 2019</p>
 	</div>
 	
 	<div class="entrada_blog">
-		<p><b><a href='experiencia_merendalia_que_son_los_menus_privados'>EXPERIENCIA MERENDALIA: ¿QUÉ SON LOS MENÚS PRIVADOS?</a></b></p>
+		<a href='experiencia_merendalia_que_son_los_menus_privados'><p class="titulo_entrada_blog"><b>EXPERIENCIA MERENDALIA: ¿QUÉ SON LOS MENÚS PRIVADOS?</b></p>
+		<img src='./views/default/img/brindis_mesa_amigos_comida.jpg'
+		title='brindis mesa amigos comida merendalia'
+		alt='brindis mesa amigos comida merendalia' class='img_articulo_blog' /></a>
 		<p>07 de marzo de 2019</p>
 	</div>
 	
 	<div class="entrada_blog">
-		<p><b><a href='que_es_merendalia_y_por_que_elegirnos'>¿QUÉ ES MERENDALIA Y POR QUÉ ELEGIRNOS?</a></b></p>
+		<a href='que_es_merendalia_y_por_que_elegirnos'><p class="titulo_entrada_blog"><b>¿QUÉ ES MERENDALIA Y POR QUÉ ELEGIRNOS?</b></p>
+		<img src='./views/default/img/copas brindis brindar amigos celebración merendalia.jpg'
+		title='copas brindis brindar amigos celebración merendalia'
+		alt='copas brindis brindar amigos celebración merendalia'
+		class='img_articulo_blog' /></a>
 		<p>24 de enero de 2019</p>
 	</div>
 	
 	<div class="entrada_blog">
-		<p><b><a href='arranque_de_las_meriendas_taller_de_merendalia'>ARRANQUE DE LAS MERIENDAS - TALLER DE MERENDALIA</a></b></p>
+		<a href='arranque_de_las_meriendas_taller_de_merendalia'><p class="titulo_entrada_blog"><b>ARRANQUE DE LAS MERIENDAS TALLER DE MERENDALIA</b></p>
+		<img src='./views/default/img/1. taller infantil merienda pinturas rotuladores.jpg'
+		title='Taller infantil merienda pinturas rotuladores'
+		alt='Talleres Infantiles merienda pinturas rotuladores'
+		class='img_articulo_blog' /></a>
 		<p>19 de enero de 2019</p>
 	</div>
 
