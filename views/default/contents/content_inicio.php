52ade43ffa45551d7a38a1804f5cb9566c194b9b
Costa <costa_torito@hotmail.com>
2020-10-29 21:23:46 +0100

    Cookies

--- views/default/contents/content_inicio.php
+++ views/default/contents/content_inicio.php
@@ -22,8 +22,7 @@
 	<div id='infobox3'>
 		<p>
 			Esta web utiliza cookies para obtener datos estadísticos de la
-			navegación de sus usuarios. Si continúas navegando consideramos que
-			aceptas su uso. <a href='cookies' class='mas_informacion'>Más
+			navegación de sus usuarios. <a href='cookies' class='mas_informacion'>Más
 				información </a> <a onclick='aceptar_cookies();'
 				style='cursor: pointer;' class='aceptar'> Aceptar</a> <a href='#'
 				class='rechazar'>Rechazar</a>
