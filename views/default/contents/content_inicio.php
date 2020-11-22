604255760d30b2eaf549b865799950f04784988b
Costa <costa_torito@hotmail.com>
2020-11-22 10:02:21 +0100

    Reseñas google

--- views/default/contents/content_inicio.php
+++ views/default/contents/content_inicio.php
@@ -17,6 +17,148 @@
 		<h2>Catering</h2> </a>
 </div>
 
+
+<div class='google'>
+
+	<h3>¿Quieres saber qué dicen nuestros clientes?</h3>
+
+	<div class='google_flex'>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<img src="./views/default/img/perfil/MJesusBahillo.png"
+						title="Mª Jesús Bahillo"> Mª Jesús Bahillo
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Ha sido una grata sorpresa descubrir Merendalia. Un espacio
+					perfecto para celebrar cualquier tipo de evento. Sin duda un gran
+					acierto poder celebrar allí mis 50! En este caso eramos unas 30
+					personas, incluido un Photocoll. Es céntrico y bien comunicado, el
+					local está decorado con buen gusto. El trato por parte de los
+					propietarios ha sido excelente. Todo han sido facilidades. El
+					catering es casero y riquísimo (tortillas y empanadas) ya que el
+					resto lo llevamos nosotros. El hecho de que tenga cocina para poder
+					preparar comida, creo que es un plus para el local. Te hace sentir
+					como en casa. Lo pasamos genial. Sin duda lo tendremos en cuenta
+					para la siguiente."</p> </a>
+
+		</div>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<i class="fa fa-user"></i> Marina Jorge Requejo
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Celebramos el cumpleaños de mi pareja en Merendalia el fin de
+					semana y no pudo salir mejor. El sitio es estupendo, céntrico y
+					acogedor. Espacioso y muy limpio. Contraté el catering de ellos que
+					es casero y muy variado además te ayudan a elegirlo y que sea
+					apropiado para el número de invitados y no quedarse corto. Se
+					adaptan a ti y nos dejaron ir a decorarlo antes de lo previsto. Sin
+					duda repetiremos."</p> </a>
+		</div>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<i class="fa fa-user"></i> Ana, Alberto y Mario
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Reservamos el local y les encargamos una merienda para una
+					celebración familiar. Canapés, mini hamburguesas, croquetas,
+					empanadas....todo casero, recién hecho, riquísimo!!! De postre
+					mousse de limón muy bueno!!!! Gran variedad de todo. Local moderno
+					y muy bonito. Muchas posibilidades y facilidades para que todo
+					salga perfecto. Éramos 24 comensales, todos sentados, muy cómodos
+					de espacio. Los responsables del negocio muy amables y cercanos.
+					Sin duda repetiremos!!!"</p> </a>
+		</div>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<img src="./views/default/img/perfil/LuciaYustosLopez.png"
+						title="Lucia Yustos Lopez"> Lucia Yustos Lopez
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Lugar perfecto para cualquier celebración! En nuestro caso
+					tuvimos bailes, picoteo, videos emotivos, música y mucha diversión!
+					El trato con los responsables impecable y dispuestos a cualquier
+					petición! En resumen GRACIAS por participar un día inolvidable!"</p>
+			</a>
+		</div>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<img src="./views/default/img/perfil/IsabelMartinez.png"
+						title="Isabel Martínez"> Isabel Martínez
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Celebramos una comida con amigos y no pudimos salir más
+					encantados. El local está súper céntrico y es muy cómodo y
+					acogedor. Dispone de minipiscina de bolas para los niños así que
+					perfecto! Se puede alquilar el local o el local y la comida.
+					Hicimos ésto último y estaba todo increíble. El menú incluye
+					pinchos fríos, calientes, el plato principal, postre y la bebida.
+					Lo sirven recién hecho, rápido y encima son súper majos. Buenísimos
+					el tartar, el risotto, las croquetas... Recomendadísimo!"</p> </a>
+		</div>
+
+		<div class='resenas_google'>
+
+			<a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">
+				<p class='nombre'>
+					<i class="fa fa-user"></i> Mercedes Fadrique
+				</p>
+				<p class='estrella'>
+					<i class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i><i class="fa fa-star"></i><i
+						class="fa fa-star"></i>
+				</p>
+				<p>"Hoy hemos probado los canapés. Los encargamos para comer en
+					casa. Han sido un éxito."</p> </a>
+		</div>
+
+	</div>
+	
+	<p class="leer_mas">Si quieres leer más , <a
+				href='https://supple.live/8nN4UVRkRp32hhWu9' target="_blank">pincha aquí</a></p>
+
+</div>
 <!--//BLOQUE COOKIES
 <div id='overbox3'>
 	<div id='infobox3'>
