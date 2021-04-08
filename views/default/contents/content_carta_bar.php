ff1de493afb7881aa7b99c852afc2ecfe235b1d7
Costa <costa_torito@hotmail.com>
2021-04-08 09:15:51 +0200

    Nueva carta bar

--- views/default/contents/content_carta_bar.php
+++ views/default/contents/content_carta_bar.php
@@ -3,7 +3,7 @@
 <div class="carta_bar">
 
 	<img
-		src='./views/default/img/CartaBar/Carta_Merendalia_Bar.jpg'
+		src='./views/default/img/CartaBar/CARTA_BAR_PRECIOS-NUEVOS.jpg'
 		title='Carta Bar'
 		alt='Carta Bar'/>
 </div>
